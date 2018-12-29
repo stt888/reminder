8dca9a1a686c0711bf7c4577bc163a67c7659827
tina <tinasun20@gmail.com>
2018-12-29 17:00:57 -0500

    Updates

---
+++ precache-manifest.d2a3ec34243bc08edb762bf2ec9ba2ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c1f8d1e35cfc77d4c2",
+    "url": "/reminder/static/js/runtime~main.a0c1f8d1.js"
+  },
+  {
+    "revision": "1f756d75572622049693",
+    "url": "/reminder/static/js/main.1f756d75.chunk.js"
+  },
+  {
+    "revision": "a4b30d0fe4febda94b0d",
+    "url": "/reminder/static/js/1.a4b30d0f.chunk.js"
+  },
+  {
+    "revision": "1f756d75572622049693",
+    "url": "/reminder/static/css/main.f79dabe8.chunk.css"
+  },
+  {
+    "revision": "3c3577af881068c74706eddc30de38c2",
+    "url": "/reminder/index.html"
+  }
+];
